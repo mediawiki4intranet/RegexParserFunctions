0d9be244eeecfa79754f2b6346f92737cfc87dc1
Vitaliy Filippov <vitalif@yourcmc.ru>
2012-02-01 16:55:17 +0000

    Bug 93182 - Update extensions, configs and tools to be compatible with 1.18 and INCOMPATIBLE with 1.16

--- RegexParserFunctions.php
+++ RegexParserFunctions.php
@@ -4,7 +4,7 @@
  * @author Jim R. Wilson
  * @version 0.1
  * @copyright Copyright (C) 2007 Jim R. Wilson
- * @license The MIT License - http://www.opensource.org/licenses/mit-license.php 
+ * @license The MIT License - http://www.opensource.org/licenses/mit-license.php
  * -----------------------------------------------------------------------
  * Description:
  *     This is a MediaWiki extension which adds a parser function for performing
@@ -22,40 +22,39 @@
  *         Initial release.
  * -----------------------------------------------------------------------
  * Copyright (c) 2007 Jim R. Wilson
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy 
- * of this software and associated documentation files (the "Software"), to deal 
- * in the Software without restriction, including without limitation the rights to 
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
- * the Software, and to permit persons to whom the Software is furnished to do 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do
  * so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all 
+ *
+ * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
- * OTHER DEALINGS IN THE SOFTWARE. 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  * -----------------------------------------------------------------------
  */
- 
+
 # Confirm MW environment
 if (defined('MEDIAWIKI')) {
 
 # Credits
 $wgExtensionCredits['parserhook'][] = array(
-    'name'=>'RegexParserFunctions',
-    'author'=>'Jim R. Wilson - wilson.jim.r&lt;at&gt;gmail.com',
-    'url'=>'http://jimbojw.com/wiki/index.php?title=RegexParserFunctions',
-    'description'=>'Adds a parser function for search and replace using regular expressions.',
-    'version'=>'0.1'
+    'name'          => 'RegexParserFunctions',
+    'author'        => 'Jim R. Wilson - wilson.jim.r&lt;at&gt;gmail.com',
+    'url'           => 'http://jimbojw.com/wiki/index.php?title=RegexParserFunctions',
+    'description'   => 'Adds a parser function for search and replace using regular expressions.',
+    'version'       => '0.1',
 );
-
 
 /**
  * Wrapper class for encapsulating Regexp related parser methods
@@ -70,14 +69,18 @@ class RegexParserFunctions {
      * @param String $replacement Regular expression replacement.
      * @return String Result of replacing pattern with replacement in string, or matching text if replacement was omitted.
      */
-    function regexParserFunction( $parser, $subject=null, $pattern=null, $replacement=null ) {
-        if ($subject===null || $pattern===null) return '';
+    function regexParserFunction( $parser, $subject = null, $pattern = null, $replacement = null ) {
+        if ( $subject === null || $pattern === null) {
+            return '';
+        }
         $acceptable = '/^([\\/\\|%]).*\\1[imsu]*$/';
-        if (!preg_match($acceptable, $pattern)) return wfMsg('regexp-unacceptable',$pattern);
-        if ($replacement===null) {
-            return (preg_match($pattern, $subject, $matches) ? $matches[0] : '');
+        if ( !preg_match( $acceptable, $pattern ) ) {
+            return wfMsg( 'regexp-unacceptable', $pattern );
+        }
+        if ( $replacement === null ) {
+            return ( preg_match( $pattern, $subject, $matches ) ? $matches[0] : '');
         } else {
-            return preg_replace($pattern, $replacement, $subject);
+            return preg_replace( $pattern, $replacement, $subject );
         }
     }
 
@@ -98,9 +101,8 @@ class RegexParserFunctions {
      */
     function regexParserFunctionSetup( ) {
         global $wgParser, $wgMessageCache;
-        $wgParser->setFunctionHook( 'regex', array($this, 'regexParserFunction') );
-        $wgParser->setFunctionHook( 'regexp', array($this, 'regexParserFunction') );
-        $wgMessageCache->addMessage('regexp-unacceptable', 'The regular expression &quot;<tt><nowiki>$1</nowiki></tt>&quot; is unacceptable.');
+        $wgParser->setFunctionHook( 'regex', array( $this, 'regexParserFunction' ) );
+        $wgParser->setFunctionHook( 'regexp', array( $this, 'regexParserFunction' ) );
     }
 }
 
@@ -108,7 +110,7 @@ class RegexParserFunctions {
 $wgRegexParserFunctions = new RegexParserFunctions();
 $wgHooks['LanguageGetMagic'][] = array($wgRegexParserFunctions, 'regexParserFunctionMagic');
 $wgExtensionFunctions[] = array($wgRegexParserFunctions, 'regexParserFunctionSetup');
+$wgExtensionMessagesFiles['RegexParserFunctions'] = dirname(__FILE__).'/RegexParserFunctions.i18n.php';
 
 } # End MW Environment wrapper
 //</source>
-?>
