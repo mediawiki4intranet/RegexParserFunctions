f9a94b19465caf5ec57c41a977247c7c917374f2
Vitaliy Filippov <vitalif@yourcmc.ru>
2012-02-16 13:29:33 +0000

    Bug 93182 - Add i18n

---
+++ RegexParserFunctions.i18n.php
@@ -0,0 +1,9 @@
+<?php
+
+// Localisation file for RegexParserFunctions extension
+
+$messages = array();
+
+$messages['en'] = array(
+    'regexp-unacceptable' => 'The regular expression &quot;<tt><nowiki>$1</nowiki></tt>&quot; is unacceptable.'
+);
