1c801c8781b321f46d9b4de09bf239239dd32934
Sophivorus <schenonef@gmail.com>
2016-09-03 18:59:00 -0300

    Update extension to registration

--- RegexParserFunctions.php
+++ RegexParserFunctions.php
@@ -1,67 +1,7 @@
 <?php
-/*
- * RegexParserFunctions.php - Allows regular expression search and replace within a string
- * @author Jim R. Wilson, Vitaliy Filippov
- * @version 2015-10-22
- * @copyright Copyright (c) 2007 Jim R. Wilson, (c) 2011+ Vitaliy Filippov
- * @license The MIT License - http://www.opensource.org/licenses/mit-license.php
- * -----------------------------------------------------------------------
- * Description:
- *     This is a MediaWiki extension which adds a parser function for performing
- *     regular expression searches and replacements.
- * Requirements:
- *     MediaWiki 1.16.x or higher
- * Installation:
- *     1. Drop this script (RegexParserFunctions.php) in $IP/extensions
- *         Note: $IP is your MediaWiki install dir.
- *     2. Enable the extension by adding this line to your LocalSettings.php:
- *         require_once('extensions/RegexParserFunctions.php');
- * -----------------------------------------------------------------------
- * Copyright (c) 2007 Jim R. Wilson
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do
- * so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- * -----------------------------------------------------------------------
- */
 
-# Confirm MW environment
-if (!defined('MEDIAWIKI'))
-    die("Not in MediaWiki environment");
-
-# Credits
-$wgExtensionCredits['parserhook'][] = array(
-    'name'          => 'RegexParserFunctions',
-    'author'        => 'Jim R. Wilson (wilson.jim.r<at>gmail.com), Vitaliy Filippov (vitalif<at>mail.ru)',
-    'url'           => 'http://wiki.4intra.net/RegexParserFunctions',
-    'description'   => 'Adds a parser function for search and replace using regular expressions.',
-    'version'       => '2015-12-02',
-);
-
-$wgHooks['LanguageGetMagic'][] = 'RegexParserFunctions::getMagic';
-$wgHooks['ParserFirstCallInit'][] = 'RegexParserFunctions::initParser';
-$wgExtensionMessagesFiles['RegexParserFunctions'] = dirname(__FILE__).'/RegexParserFunctions.i18n.php';
-
-/**
- * Wrapper class for encapsulating Regexp related parser methods
- */
-class RegexParserFunctions
-{
+class RegexParserFunctions {
+ 
     /**
      * Performs regular expression search or replacement.
      *
